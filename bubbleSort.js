15453effd629c41ace3dc8844fa5faeac4aa8d5d
HyoEun Lee <hyoeun93@gmail.com>
2020-01-15 09:49:13 -0500

    bubbleSorted

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,3 +1,15 @@
 let bubbleSort = function(array) {
-    
+    let isSorted = false;
+    while(!isSorted) {
+        isSorted = true;
+        for(let i = 0; i < array.length - 1; i++) {
+            if(array[i] > array[i + !]) {
+                let temp = array[i]
+                array[i] = array[i + 1]
+                array[i + 1] = temp;
+                isSorted = false;
+            }
+        }
+    }
+    return array;
 }
